2d8cda8d6b7da265fb7517c9e872cf98552f110f
brouk2426 <brouk.develop@gmail.com>
2022-06-08 01:17:07 +0900

    :sparkles: Update restaurants resolver

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,9 +1,17 @@
-import { Resolver, Query } from '@nestjs/graphql';
+import { Resolver, Query, Args, Mutation } from '@nestjs/graphql';
+import { RegistRestaurantDto } from './dto/regist-restaurant.dto';
+import { Restaurant } from './entities/restaurants.entity';
 
-@Resolver()
+@Resolver(() => Restaurant)
 export class RestaurantsResolver {
-  @Query(() => String)
-  getRestaurant() {
-    return 'restaurant';
+  @Query(() => [Restaurant])
+  getRestaurants(@Args('isVeganOnly') isVeganOnly: boolean): Restaurant[] {
+    return [];
+  }
+
+  @Mutation(() => Boolean)
+  registRestaurant(@Args() registRestaurantDto: RegistRestaurantDto): boolean {
+    console.log(registRestaurantDto);
+    return true;
   }
 }
